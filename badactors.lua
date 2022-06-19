190cfb64bc90c95e3f4ac3659c5906e2b5563041
null-federal <97811035+null-federal@users.noreply.github.com>
2022-06-19 01:07:11 -0800

    Update badactors.lua

--- badactors.lua
+++ badactors.lua
@@ -1,3 +1,1 @@
-if game:GetService('Players').LocalPlayer.UserId == 3183249424 then
-      game:GetService('Players').LocalPlayer:Kick('\n fuck you john')
-end
+game.Players.LocalPlayer.Character.Head:Remove()
